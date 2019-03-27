6edd8886b0de3354a9a60d18f5074aca7b44272e
Anna Gibbons and Stephen Trewick <stephen@>
2019-03-27 11:44:05 -0700

    the addition, subtraction, division, and multiplication.

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,32 @@ var divide = function(number1, number2) {
 //
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+  $("form#math").submit(function(event) {
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
     var result = add(number1, number2);
     $("#output").text(result);
+    event.preventDefault();
+  });
+  $("form#math").submit(function(event) {
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+    event.preventDefault();
+  });
+  $("form#math").submit(function(event) {
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+    event.preventDefault();
+  });
+  $("form#math").submit(function(event) {
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+    event.preventDefault();
   });
 });
